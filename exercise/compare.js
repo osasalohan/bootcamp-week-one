6f6edc4ff7004354d3e6fd17092d58473872cf82
Jerryodeghe <odeghejerry@gmail.com>
2020-11-16 11:38:24 +0100

    exercise

---
+++ exercise/compare.js
@@ -0,0 +1,10 @@
+/**
+ * Write a function that compares two value for strict equlity
+ * The function takes in two values. The first value is the string to be caompare
+ * and the second value is the has value to compare against
+ *
+ * You function must cover all edge cases
+ *
+ *
+ * Tip: use the crypto module in nodeJS
+ * */
---
+++ exercise/hash.js
@@ -0,0 +1,7 @@
+/**
+ * Write a function that hash a string value
+ * You function must cover all edge cases
+ *
+ *
+ * Tip: use the crypto module in nodeJS
+ * */
