3561aef38d56d22344193acbddda129b29e3220a
osasalohan <58654983+osasalohan@users.noreply.github.com>
2020-11-17 16:52:49 +0100

    add documentation

--- models/nodes.js
+++ models/nodes.js
@@ -1,9 +1,15 @@
+/**
+ * To represent self closing html elements that contain attributes only
+ */
 exports.AttributeNode = class AttributeNode {
   constructor(attributes = []) {
     this.attributes = attributes;
   }
 };
 
+/**
+ * To  represent html elements that can contain attributes, text and other elements
+ */
 exports.ElementNode = class ElementNode {
   constructor(attributes = [], descendants = [], text = "") {
     this.attributes = ["class", ...attributes];
@@ -12,12 +18,18 @@ exports.ElementNode = class ElementNode {
   }
 };
 
+/**
+ * To represent html elements that can contain only text
+ */
 exports.TextNode = class TextNode {
   constructor(text = "") {
     this.text = text;
   }
 };
 
+/**
+ * To represent html elements that only contain other elements (head and body)
+ */
 exports.DocumentNode = class DocumentNode {
   constructor(descendants = []) {
     this.descendants = descendants;
